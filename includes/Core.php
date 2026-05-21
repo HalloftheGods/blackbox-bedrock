9932886d8197d066a9c545ffa16d4390171ed57d
Xopher <x@hallofthegods.com>
2026-05-21 03:34:36 -0700

    feat: implement universal glass UI transparency for WooCommerce admin and force core theme support

--- includes/Core.php
+++ includes/Core.php
@@ -9,4 +9,13 @@ class Core {
 		$js = file_exists( $js_path ) ? file_get_contents( $js_path ) : '';
 		echo '<script id="blackbox-smoke-canvas-js">' . $js . '</script>';
 	}
+
+	public static function force_woocommerce_theme_support() {
+		add_theme_support( 'woocommerce' );
+		add_theme_support( 'wc-product-gallery-zoom' );
+		add_theme_support( 'wc-product-gallery-lightbox' );
+		add_theme_support( 'wc-product-gallery-slider' );
+	}
 }
+
+add_action( 'after_setup_theme', [ '\BlackBOX\Core', 'force_woocommerce_theme_support' ], 999 );
